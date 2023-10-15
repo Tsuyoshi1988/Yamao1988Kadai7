36c7e58c820f478b1d0c4986809ea0b0ff52da2b
Tsuyoshi Kamada <ms.2bpd.cch@gmail.com>
2023-10-15 11:22:02 +0900

    課題７をコミット！

---
+++ 課題7/GreenViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GreenViewController.swift
+//  課題7
+//
+//  Created by 鎌田剛史 on R 5/07/23.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func pressButton(_ sender: Any) {
+        let num1 = ((textField.text ?? "") as NSString).integerValue
+        let num2 = ((textField2.text ?? "") as NSString).integerValue
+        
+        let result = num1 - num2
+        label.text = "\(result)"
+        
+        self.textField.resignFirstResponder()
+        self.textField2.resignFirstResponder()
+    }
+}
